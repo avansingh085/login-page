6f7b2e0af4a93a0da112a6c633a35df140f3de06
Avan Singh <137961786+avansingh085@users.noreply.github.com>
2023-10-27 21:23:38 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,19 +1,13 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: '12345',      // Replace with your database password
-  database: 'mydb' // // Replace with your database Name
+  host: 'localhost', 
+  user: 'root',     
+  password: '12345',  
+  database: 'mydb' 
 }); 
  
 conn.connect(function(err) {
   if (err) throw err;
-  //console.log('Database is connected successfully !');
+  
 });
-
-// conn.query("select*from avan",(err,res)=>{
-//     if(err)
-//     throw err;
-// console.log(res);
-// })
 module.exports = conn;
